547c81ef99fca27f1b1453d4a6f471b753ec4e48
DengQi <dengqi935@gmail.com>
2018-12-05 22:24:16 +0800

    spacevim config

---
+++ SpaceVim/README.md
@@ -0,0 +1,10 @@
+my configs for space vim
+
+
+## note:
+
+VimProc need to use gcc to compile, you may find it in `~/.cache/...`.
+
+Modify the `make_mac.mak`.
+
+
---
+++ SpaceVim/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+## For lsp
+npm install -g javascript-typescript-langserver
+npm install vue-language-server -g
+npm i -g typescript-language-server
+npm i -g bash-language-server
+
+## For markdown
+
+npm -g install remark
+npm -g install remark-cli
+npm -g install remark-stringify
+
+## For python
+
+pip install --user flake8
+pip install --user yapf
+pip install --user yapf
+pip install --user isort
+
+
+
