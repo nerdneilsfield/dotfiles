df26d9e546aca6c94afc9ded5aba41fa266acec1
dengqi <dengqi935@gmail.com>
2021-11-22 17:47:34 +0800

    feat: add GenOpenSSHBat for wsl to open ssh in windows host

--- zsh/.config/zsh/config.wsl.zsh
+++ zsh/.config/zsh/config.wsl.zsh
@@ -1,5 +1,15 @@
 alias exp='explorer.exe'
 
+function GenOpenSSHBat() {
+ HOST_IP="$(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | grep 172 | awk '{print $2}' | head -n 1)"
+ HOST_PORT="$(cat /etc/ssh/sshd_config | grep ^Port | awk '{print $2}' | tail -n1)"
+ CODENAME=$(lsb_release -c | awk '{print $2}')
+ 
+ tee ~/setup_wsl_ssh.bat &>/dev/null << EOF
+netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=2222 connectaddress=${HOST_IP} connectport=${HOST_PORT}
+netsh advfirewall firewall add rule name=${HOST}_${CODENAME} dir=in action=allow protocol=TCP localport=${HOST_PORT}
+EOF
+}
 
 # # color list: https://upload.wikimedia.org/wikipedia/commons/1/15/Xterm_256color_chart.svg
 # zstyle :prompt:pure:path color 241
