f961b969679704034664ca27f7bdc77edbfbdee2
dengqi <dengqi935@gmail.com>
2022-05-17 10:23:36 +0800

    zsh: add pyenv and poetry configurations

--- zsh/.config/zsh/python.zsh
+++ zsh/.config/zsh/python.zsh
@@ -2,3 +2,26 @@ alias python='python3'
 alias pip='python3 -m pip'
 alias pipu="python3 -m pip -U pip"
 alias addpwd2pythonpah="export PYTHONPATH=${PWD}:$PYTHONPATH"
+
+install_pyenv() {
+        rm -rf ${HOME}/.local/pyenv
+        git clone https://github.com/pyenv/pyenv.git ~/.local/pyenv
+        cd ~/.local/pyenv
+        ./src/configure
+        make -C src
+}
+
+install_poetry() {
+        rm -rf ${HOME}/.local/poetry
+        curl -sS https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py -o /tmp/get-poetry.py
+        POETRY_HOME=${HOME}/.local/poetry python3 /tmp/get-poetry.py --no-modify-path -y -f
+}
+# setup pyenv
+export PYENV_ROOT="$HOME/.local/pyenv"
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+
+#setup poetry
+export POETRY_HOME="$HOME/.local/poetry"
+command -v poetry >/dev/null || export PATH="$POETRY_HOME/bin:$PATH"
+source ${HOME}/.local/poetry/env
