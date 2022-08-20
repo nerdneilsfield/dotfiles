1429d67cec4aa28a14f11a44c180930a6c8924e9
dengqi <dengqi935@gmail.com>
2022-08-21 00:28:07 +0800

    nvchad: add nvchard custom config

---
+++ NvChad/.config/nvim/lua/custom/plugins/configs/cmp.lua
@@ -0,0 +1,11 @@
+return {
+    -- preselect = cmp.PreselectMode.None,
+    sources = {
+       { name = "copilot" },
+       { name = "nvim_lsp" },
+       { name = "luasnip" },
+       { name = "buffer" },
+       { name = "nvim_lua" },
+       { name = "path" },
+    },
+ }
---
+++ NvChad/.config/nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,63 @@
+return {
+    ["neovim/nvim-lspconfig"] = {
+        config = function()
+            require "plugins.configs.lspconfig"
+            require "custom.plugins.configs.lspconfig"
+        end,
+    },
+    ["zbirenbaum/copilot.lua"] = {
+        config = function()
+            vim.defer_fn(function()
+                require("copilot").setup {
+                    cmp = {
+                        enabled = true,
+                        method = "getCompletionsCycling",
+                    },
+                }
+            end, 100)
+        end,
+    },
+    ["zbirenbaum/copilot-cmp"] = {},
+    -- ["liuchengxu/vista.vim"] = {},
+    ["jose-elias-alvarez/null-ls.nvim"] = {
+        after = "nvim-lspconfig",
+        config = function()
+            require "custom.plugins.configs.null-ls"
+        end,
+    },
+    ["folke/which-key.nvim"] = {
+        disable = false,
+        keys = { '<leader>' , '<localleader>' },
+    },
+
+    ["williamboman/mason.nvim"] = {
+        ensure_installed = {
+            -- lua stuff
+            "lua-language-server",
+            "stylua",
+
+            -- web dev
+            --   "css-lsp",
+            --   "html-lsp",
+            --   "typescript-language-server",
+            --   "deno",
+            --   "emmet-ls",
+            "json-lsp",
+
+            -- shell
+            "shfmt",
+            "shellcheck",
+
+            -- c++/clang
+            "clang-format",
+            "clangd",
+            "cmake-language-server",
+            "cmakelang",
+            "cpplint",
+            "cpptools",
+
+            ---- zls
+            "zls"
+        },
+    },
+}
