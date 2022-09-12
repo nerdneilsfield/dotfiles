c739e419cf1aaf559387d725b0bef48fe5935090
Dengqi <me@dengqi.org>
2022-09-12 23:28:23 +0800

    init: install quick install

--- init/init_ubuntu_user.sh
+++ init/init_ubuntu_user.sh
@@ -35,6 +35,7 @@ function InstallNvm() {
 function InstallRust() {
 	echo "----install rustup ------"
 	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+  cargo install quickinstall
 }
 
 function InstallGoTools() {
