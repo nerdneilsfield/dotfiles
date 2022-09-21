00c5d2d7c9b5d9ed663129b7822e8f00a29dc481
dengqi <dengqi935@gmail.com>
2022-09-22 05:13:33 +0800

    zsh: ros: add init ros2

--- zsh/.config/zsh/ros.zsh
+++ zsh/.config/zsh/ros.zsh
@@ -4,10 +4,12 @@
 # alias catim="catkin_make_isolated --use-ninja --install -DCMAKE_EXPORT_COMPILE_COMMANDS=ON"
 # alias catimp="cd ../../ & catkin_make_isolated  --use-ninja --install -DCMAKE_EXPORT_COMPILE_COMMANDS=ON"
 
-alias catm="catkin_make  --use-ninja  -DCMAKE_EXPORT_COMPILE_COMMANDS=ON"
-alias catmp="cd ../../ & catkin_make  --use-ninja  -DCMAKE_EXPORT_COMPILE_COMMANDS=ON"
-alias catim="catkin_make_isolated --use-ninja  -DCMAKE_EXPORT_COMPILE_COMMANDS=ON"
-alias catimp="cd ../../ & catkin_make_isolated  --use-ninja  -DCMAKE_EXPORT_COMPILE_COMMANDS=ON"
+alias catm="catkin build  --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON"
+alias catmr="catkin build  --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON"
+alias catmd="catkin build  --cmake-args -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON"
+alias catmn="catkin build --cmake-args  -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -G Ninja"
+# alias catim="catkin_make_isolated --use-ninja  -DCMAKE_EXPORT_COMPILE_COMMANDS=ON"
+# alias catimp="cd ../../ & catkin_make_isolated  --use-ninja  -DCMAKE_EXPORT_COMPILE_COMMANDS=ON"
 
 
 #alias init_ros="source /opt/ros/melodic/setup.zsh"
@@ -27,6 +29,25 @@ init_ros() {
 	esac
 }
 
+init_ros2() { 
+	CODENAME=$(lsb_release -c | awk '{print $2}')
+	case $CODENAME in
+		bionic)
+			source /opt/ros/eloquent/setup.zsh
+			;;
+		focal)
+			source /opt/ros/foxy/setup.zsh
+			;;
+		*)
+			echo "Unknown codename $CODENAME"
+			;;
+	esac
+}
+
 init_current_ros() {
 	source devel/setup.zsh
+}
+
+init_current_ros2() {
+	source install/local_setup.zsh
 }
