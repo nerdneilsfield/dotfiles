a4cb90c42d25e38367ef547c501174615b76cac0
dengqi <dengqi935@gmail.com>
2022-09-22 05:14:01 +0800

    zsh:shell: add some shell tools

--- zsh/.config/zsh/shell.zsh
+++ zsh/.config/zsh/shell.zsh
@@ -1,1 +1,20 @@
 
+install_shfmt() {
+        # shfmt
+        go install mvdan.cc/sh/v3/cmd/shfmt@latest
+}
+
+install_bash_lsp() {
+        npm i -g bash-language-server
+}
+
+install_shell_check(){
+        pki shellcheck
+}
+
+install_shell_tools() {
+        install_shfmt
+        install_bash_lsp
+        install_shell_check
+}
+
