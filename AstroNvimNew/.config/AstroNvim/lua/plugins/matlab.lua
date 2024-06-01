1469e0717bd38bdbf9624cacf03050282c6c2ab0
dengqi <me@dengqi.org>
2024-06-01 23:53:04 +0800

    astro: add matlab

---
+++ AstroNvimNew/.config/AstroNvim/lua/plugins/matlab.lua
@@ -0,0 +1,64 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "matlab" })
+      end
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "matlab_ls" })
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "matlab_ls" })
+    end,
+  },
+  -- {
+  --   "jay-babu/mason-null-ls.nvim",
+  --   optional = true,
+  --   opts = function(_, opts)
+  --     opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "shfmt" })
+  --     opts.sources = {
+  --       require("null-ls").builtins.formatting.shfmt.with { filetypes = { "sh", "bash", "zsh" } },
+  --     }
+  --   end,
+  -- },
+  -- {
+  --   "jay-babu/mason-nvim-dap.nvim",
+  --   optional = true,
+  --   opts = function(_, opts)
+  --     opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "bash" })
+  --   end,
+  -- },
+  -- {
+  --   "stevearc/conform.nvim",
+  --   optional = true,
+  --   opts = {
+  --     formatters_by_ft = {
+  --       sh = { "shfmt" },
+  --       bash = { "shfmt" },
+  --       zsh = { "shfmt" },
+  --     },
+  --   },
+  -- },
+  -- {
+  --   "mfussenegger/nvim-lint",
+  --   optional = true,
+  --   opts = {
+  --     linters_by_ft = {
+  --       sh = { "shellcheck" },
+  --       bash = { "shellcheck" },
+  --       zsh = { "shellcheck" },
+  --     },
+  --   },
+  -- },
+}
