0ee9edc70afccac2a66683373584eab456cc9944
dengqi <dengqi935@gmail.com>
2024-06-03 14:53:33 +0800

    powershell: update zsh

--- windows-powershell/.powershell/powershell.ps1
+++ windows-powershell/.powershell/powershell.ps1
@@ -1,11 +1,13 @@
-function Install-Powershell-Soft {
+function Install-Powershell-Soft
+{
     $powershell_soft=@(
         "starship",
         "zoxide",
         "fzf"
     )
 
-    foreach ($soft in $powershell_soft) {
+    foreach ($soft in $powershell_soft)
+    {
         scoop install $soft
     }
 
@@ -15,15 +17,39 @@ function Install-Powershell-Soft {
         "PSReadLine",
         "PSFzf"
     )
-    foreach ($module in $powershell_modules) {
+    foreach ($module in $powershell_modules)
+    {
         Install-Module -Name $module -Force
     }
 }
 
-if (Get-Command starship -ErrorAction SilentlyContinue) {
+if (Get-Command starship -ErrorAction SilentlyContinue)
+{
     Invoke-Expression (&starship init powershell)
 }
 
-if (Get-Command zoxide -ErrorAction SilentlyContinue) {
+if (Get-Command zoxide -ErrorAction SilentlyContinue)
+{
     Invoke-Expression (& { (zoxide init powershell | Out-String) })
 }
+
+# if installed module PSFzf PSReadLine then set up fzf key bindings
+if (Get-Module -ListAvailable PSFzf -ErrorAction SilentlyContinue)
+{
+    Import-Module PSFzf -Scope CurrentUser -Force
+}
+
+if (Get-Module -ListAvailable PSReadLine -ErrorAction SilentlyContinue)
+{
+    Import-Module PSReadLine -Scope CurrentUser -Force
+}
+
+if (Get-Module -ListAvailable PSReadLine -ErrorAction SilentlyContinue)
+{
+    Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+
+    Set-PSReadLineKeyHandler -Chord 'Ctrl+r' -ScriptBlock {
+        Invoke-FuzzyHistory
+    }
+}
+
--- windows-powershell/.powershell/tools.ps1
+++ windows-powershell/.powershell/tools.ps1
@@ -1,4 +1,5 @@
-function Install-Basic-Tools {
+function Install-Basic-Tools
+{
     $tools=@(
         "fzf",
         "ripgrep",
@@ -49,13 +50,15 @@ function Install-Basic-Tools {
         "qsv",
         "dufs"
     )
-    foreach ($tool in $tools) {
+    foreach ($tool in $tools)
+    {
         Green-Echo "=======install $tool========="
         scoop install $tool
     }
 }
 
-function Install-Basic-Tools-In-Rust {
+function Install-Basic-Tools-In-Rust
+{
     $tools=@(
         "rm-improved",
         "bandwhich",
@@ -64,13 +67,15 @@ function Install-Basic-Tools-In-Rust {
         "choose",
         "hck"
     )
-    foreach ($tool in $tools) {
+    foreach ($tool in $tools)
+    {
         Green-Echo "=======install $tool========="
         cargo binstall $tool
     }
 }
 
-function Install-Gui-Tools {
+function Install-Gui-Tools
+{
     $tools=@(
         "putty",
         "winscp",
@@ -83,14 +88,16 @@ function Install-Gui-Tools {
         "Flow-Launcher"
     )
 
-    foreach ($tool in $tools) {
+    foreach ($tool in $tools)
+    {
         Green-Echo "=======install $tool========="
         scoop install $tool
     }
 }
 
 
-function Extract-7Zip {
+function Extract-7Zip
+{
     param (
         [string]$in,  # 压缩文件路径
         [string]$out,     # 输出目录
@@ -101,13 +108,15 @@ function Extract-7Zip {
     $sevenZipExecutable = "7z.exe"
 
     # 检查 7-Zip 可执行文件是否存在
-    if (-Not (Get-Command $sevenZipExecutable -ErrorAction SilentlyContinue)) {
+    if (-Not (Get-Command $sevenZipExecutable -ErrorAction SilentlyContinue))
+    {
         Write-Error "7-Zip 可执行文件未找到，请检查路径：$sevenZipExecutable"
         return
     }
 
     # 创建输出目录（如果不存在）
-    if (-Not (Test-Path $out)) {
+    if (-Not (Test-Path $out))
+    {
         New-Item -ItemType Directory -Path $out | Out-Null
     }
 
@@ -115,15 +124,18 @@ function Extract-7Zip {
     & $sevenZipExecutable x "$in" -o"$out" -p$password -y
 
     # 检查解压缩结果
-    if ($LASTEXITCODE -eq 0) {
+    if ($LASTEXITCODE -eq 0)
+    {
         Green-Echo "文件解压缩成功：$in 到 $out"
-    } else {
+    } else
+    {
         Write-Error "文件解压缩失败：$in"
     }
 }
 
 # 定义函数调用 7-Zip 压缩文件或文件夹
-function Compress-7Zip {
+function Compress-7Zip
+{
     param (
         [string]$in,  # 要压缩的文件或文件夹路径
         [string]$out,  # 输出压缩文件路径
@@ -133,14 +145,16 @@ function Compress-7Zip {
     # 7-Zip 可执行文件的路径
     $sevenZipExecutable = "7z.exe"
 
-        # 检查 7-Zip 可执行文件是否存在
-    if (-Not (Get-Command $sevenZipExecutable -ErrorAction SilentlyContinue)) {
+    # 检查 7-Zip 可执行文件是否存在
+    if (-Not (Get-Command $sevenZipExecutable -ErrorAction SilentlyContinue))
+    {
         Write-Error "7-Zip 可执行文件未找到，请检查路径：$sevenZipExecutable"
         return
     }
 
     # 检查源文件或文件夹是否存在
-    if (-Not (Test-Path $in)) {
+    if (-Not (Test-Path $in))
+    {
         Write-Error "源文件或文件夹未找到：$in"
         return
     }
@@ -149,9 +163,11 @@ function Compress-7Zip {
     & $sevenZipExecutable a "$out" "$in" -p$password -y
 
     # 检查压缩结果
-    if ($LASTEXITCODE -eq 0) {
+    if ($LASTEXITCODE -eq 0)
+    {
         Green-Echo "文件或文件夹压缩成功：$in 到 $out"
-    } else {
+    } else
+    {
         Write-Error "文件或文件夹压缩失败：$in"
     }
 }
