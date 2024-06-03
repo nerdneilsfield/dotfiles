fbc812f0265ae51b9a1c330c9c3f62fd40898ea4
dengqi <me@dengqi.org>
2024-06-03 15:28:28 +0800

    zsh: show-help: add vim help

--- zsh/.config/zsh/help.zsh
+++ zsh/.config/zsh/help.zsh
@@ -1,7 +1,7 @@
 function show-help() {
-    if command -v mdcat &> /dev/null; then
+    if command -v mdcat &>/dev/null; then
         local file
-        file=$(find "${ZSH_CONF_DIR}/help" -name '*.md' -type f | sed 's|.*/||;s|\.md$||' | fzf --prompt="Select help file: ")
+        file=$(find "${ZSH_CONF_DIR}/help" -name '*.md' -type f | sed 's|.*/||;s|\.md$||' | fzf --prompt="Select help file: " --preview='mdcat "${ZSH_CONF_DIR}"/help/{}.md')
         if [[ -n "$file" ]]; then
             mdcat "${ZSH_CONF_DIR}/help/${file}.md"
         fi
@@ -11,13 +11,13 @@ function show-help() {
     fi
 }
 
-function create-help(){
-  if [ -z "$1" ]; then
-    red_echo "Please provide a help file name."
-    return 1
-  fi
-  touch "${ZSH_CONF_DIR}/help/${1}.md"
-  nv "${ZSH_CONF_DIR}/help/${1}.md"
+function create-help() {
+    if [ -z "$1" ]; then
+        red_echo "Please provide a help file name."
+        return 1
+    fi
+    touch "${ZSH_CONF_DIR}/help/${1}.md"
+    nv "${ZSH_CONF_DIR}/help/${1}.md"
 }
 
 function edit-help() {
