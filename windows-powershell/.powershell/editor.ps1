6a88f6583119c3c67e0237bd22173ee05f65bbbb
dengqi <dengqi935@gmail.com>
2024-06-14 19:17:53 +0800

    powershell: update editor.ps1

--- windows-powershell/.powershell/editor.ps1
+++ windows-powershell/.powershell/editor.ps1
@@ -1,8 +1,17 @@
-function Install-Command-Editor {
+function Install-Command-Editor
+{
     scoop install vim neovim-nightly helix nano -s
+    scoop update vim neovim-nightly helix nano -s
 }
 
-function Install-Gui-Editor {
-    scoop install vscode sublime-text emditor typora notepadplusplus vscode-insiders
+function Install-Gui-Editor
+{
+    scoop install vscode sublime-text emeditor typora notepadplusplus vscode-insiders notepadnext
+    scoop update vscode sublime-text emeditor typora notepadplusplus vscode-insiders notepadnext
 }
 
+function Install-Typst
+{
+    scoop install typst
+    scoop update typst
+}
